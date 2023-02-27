73058814b81ac2619ef433cbd17a6fdfbf87bc86
Masum Billah <masum.27cse.ru@gmail.com>
2023-02-27 19:32:09 +0600

    NRZ_L

---
+++ NRZ_L.m
@@ -0,0 +1,36 @@
+clc;
+clear all;
+close all;
+
+data = [1 0 0 1 1 0 1 0];
+point = 100;
+encoded_signal = zeros(1,length(data)*point);
+
+for i=1:length(data)
+    if data(i)==1
+        encoded_signal((i-1)*point+1:i*point) = -1;
+    else
+        encoded_signal((i-1)*point+1:i*point) = 1;
+    end
+end
+
+size = 0:1/point:length(data)-1/point;
+
+subplot(2,1,1);
+plot(size,encoded_signal);
+title('Encoded Signal');
+ylim([-3,3]);
+
+decoded_signal = zeros(1,length(encoded_signal));
+for i=1:length(encoded_signal)
+    if encoded_signal(i)==-1
+        decoded_signal(i) = 1;
+    else
+        decoded_signal(i) = 0;
+    end
+end
+    
+subplot(2,1,2);
+plot(size,decoded_signal);
+title('Decoded Signal');
+ylim([-3,3]);
