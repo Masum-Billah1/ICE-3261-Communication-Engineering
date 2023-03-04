530741437b61af5cfd48b36460538107ef0c030f
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2023-03-04 08:58:39 +0600

    MLT-3

---
+++ MLT-3.m
@@ -0,0 +1,49 @@
+clc;
+clear all;
+close all;
+data = [0 1 0 1 1 0 1 1];
+point = 100;
+status = 1;
+flag = [1 0 -1 0];
+
+%Encoded_Signal
+%previous one : negative
+% 0 -> same as before
+% 1 -> +1,0,-1/-1,0,+1.
+status = 0;
+index = 1;
+encoded_signal = zeros(1,length(data)*point);
+for i=1:length(data)
+    if data(i) == 1
+        status = flag(index)
+        encoded_signal((i-1)*point+1:i*point) = status;
+        index = mod(index,4)+1;
+    else
+        encoded_signal((i-1)*point+1:i*point) = status;
+    end
+    
+end
+
+size = 0:1/point:length(data)-1/point;
+
+subplot(2,1,1);
+plot(size,encoded_signal);
+title('Encoded Signal');
+ylim([-3,3]);
+
+%Decoded_Signal
+%status same -> 0
+%status change -> 1
+
+status = 0;
+decoded_signal = zeros(1,length(data));
+for i=1:length(data)
+    if encoded_signal(i*point)==status
+           decoded_signal(i) = 0;
+    else
+        decoded_signal(i) = 1;
+        status = encoded_signal(i*point);
+    end
+end
+    
+decoded_signal
